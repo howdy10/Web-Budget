3ab4bdf26b84aa9c1a8d4e28c0e770795c5c6345
Howdy <eagles5_5@hotmail.com>
2021-02-25 14:41:23 -0700

    Added faker.js Created mock service path down graded material table to 1.67

--- src/Services/ExpressService.js
+++ src/Services/ExpressService.js
@@ -5,9 +5,39 @@ export default class ExpressService extends React.Component {
   constructor(props) {
     super(props);
     this.baseUrl = 'http://localhost:4000/';
+    this.fake = true;
+    this.faker = require('faker');
   }
 
   async getTransactions() {
+    if (this.fake) {
+      return Promise.resolve([
+        {
+          id: 1,
+          name: this.faker.company.companyName(),
+          date: this.faker.date.past().toString(),
+          amount: this.faker.finance.amount(),
+          notes: this.faker.lorem.words(),
+          categories: [this.faker.lorem.word(), this.faker.lorem.word()],
+        },
+        {
+          id: 2,
+          name: this.faker.company.companyName(),
+          date: this.faker.date.past().toString(),
+          amount: this.faker.finance.amount(),
+          notes: this.faker.lorem.words(),
+          categories: [],
+        },
+        {
+          id: 3,
+          name: this.faker.company.companyName(),
+          date: this.faker.date.past().toString(),
+          amount: this.faker.finance.amount(),
+          notes: this.faker.lorem.words(),
+          categories: [this.faker.lorem.word(), this.faker.lorem.word()],
+        },
+      ]);
+    }
     return axios
       .get(this.baseUrl + 'transaction/')
       .then((response) => response.data)
@@ -16,6 +46,9 @@ export default class ExpressService extends React.Component {
       });
   }
   async getAccount() {
+    if (this.fake) {
+      return Promise.resolve({ amount: this.faker.finance.amount() });
+    }
     return axios
       .get(this.baseUrl + 'account/603676bcefa592daf1dfe668')
       .then((response) => response.data)
